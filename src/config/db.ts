0503e08095db8aca59602ada4547370541959cb9
antonioTorrejon <antonioTorrejon81@gmail.com>
2024-10-16 10:42:03 +0200

    first commit

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,10 +1,17 @@
-import { Sequelize } from "sequelize-typescript"
-import dotenv from 'dotenv'
-dotenv.config()
-
+import { Sequelize } from 'sequelize-typescript';
+import * as dotenv from 'dotenv';
+ 
+// process.loadEnvFile('./.env.local');
+dotenv.config({ path: './.env.local' });
+ 
 const db = new Sequelize(process.env.DATABASE_URL!, {
-    models: [__dirname + '/../models/**/*'],
-    logging: false
-})
-
-export default db
+  models: [__dirname + '/../models/**/*'],
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+    },
+  },
+});
+ 
+export default db;
